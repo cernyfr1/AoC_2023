51316f78824524146292991c8abb4550d8915f1d
cernyfr1 <fa.cerny@gmail.com>
2023-12-06 01:09:14 +0100

    day 2 solved

---
+++ day2/puzzle1.java
@@ -0,0 +1,73 @@
+package day2;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class puzzle1 {
+
+    public static void main(String[] args) throws Exception{
+
+        ArrayList<Game> games = new ArrayList<>();
+        int result = 0;
+
+        File input = new File("day2/input.txt").getAbsoluteFile();
+        BufferedReader br = new BufferedReader(new FileReader(input));
+        br.lines().forEach(game -> games.add(new Game(game)));
+
+        ArrayList<Game> possibleGames = new ArrayList<>();
+        for (Game game : games) {
+            if (game.maxRed <= 12 && game.maxGreen <= 13 && game.maxBlue <= 14){
+                possibleGames.add(game);
+            }
+        }
+        for (Game game : possibleGames) {
+            result += game.id;
+        }
+        System.out.println(result);
+    }
+    private static class Game {
+
+        public int id;
+        public int maxRed;
+        public int maxGreen;
+        public int maxBlue;
+
+        public Game(String game) {
+
+            String[] arr = game.split(":");
+            String[] sets = arr[1].split(";");
+
+            id = Integer.parseInt(Arrays.stream(arr[0].split(" ")).toList().get(1));
+            maxRed = 0;
+            maxGreen = 0;
+            maxBlue = 0;
+
+            for (String set : sets) {
+                String[] cubes = set.split(",");
+                for (String cube : cubes) {
+                    if (cube.contains("red")){
+                        maxRed = Math.max(extractInt(cube), maxRed);
+                    }
+                    if (cube.contains("green")){
+                        maxGreen = Math.max(extractInt(cube), maxGreen);
+                    }
+                    if (cube.contains("blue")){
+                        maxBlue = Math.max(extractInt(cube), maxBlue);
+                    }
+                }
+            }
+        }
+
+        private int extractInt(String str) {
+            str = str.replaceAll("[^0-9]", ""); // regular expression
+            str = str.replaceAll(" +", "");
+            if (str.isEmpty())
+                return -1;
+
+            return Integer.parseInt(str);
+        }
+    }
+}
